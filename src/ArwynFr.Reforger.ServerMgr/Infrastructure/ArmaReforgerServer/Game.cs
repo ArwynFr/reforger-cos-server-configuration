5cac378206ca5ec5fd49f529d09819701c43be8a
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2026-06-27 08:19:04 +0200

    fix: add crossPlatform support in config

--- src/ArwynFr.Reforger.ServerMgr/Infrastructure/ArmaReforgerServer/Game.cs
+++ src/ArwynFr.Reforger.ServerMgr/Infrastructure/ArmaReforgerServer/Game.cs
@@ -6,6 +6,7 @@ public record Game(
     [property: JsonPropertyName("name")] string Name,
     [property: JsonPropertyName("password")] string Password,
     [property: JsonPropertyName("passwordAdmin")] string PasswordAdmin,
+    [property: JsonPropertyName("crossPlatform")] bool CrossPlatform,
     [property: JsonPropertyName("admins")] IReadOnlyList<string> Admins,
     [property: JsonPropertyName("scenarioId")] string ScenarioId,
     [property: JsonPropertyName("maxPlayers")] int MaxPlayers,
